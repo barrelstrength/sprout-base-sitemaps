14bc7c9bd543ae5b06c5bef69664ac088f088b0e
Ben Parizek <ben.parizek@gmail.com>
2020-04-21 22:55:21 -0400

    Fixes migration namespace

--- src/migrations/m190412_000000_recheck_default_settings.php
+++ src/migrations/m190412_000000_recheck_default_settings.php
@@ -2,35 +2,22 @@
 
 namespace barrelstrength\sproutbasesitemaps\migrations;
 
-use barrelstrength\sproutsitemaps\migrations\Install as SproutBaseSitemapsInstallMigration;
+use barrelstrength\sproutbasesitemaps\migrations\Install as SproutBaseSitemapsInstallMigration;
 use craft\db\Migration;
 use craft\errors\SiteNotFoundException;
-use yii\base\ErrorException;
 use yii\base\Exception;
-use yii\base\NotSupportedException;
-use yii\web\ServerErrorHttpException;
 
-/**
- * m190412_000000_recheck_default_settings migration.
- */
 class m190412_000000_recheck_default_settings extends Migration
 {
     /**
      * @return bool
      * @throws SiteNotFoundException
-     * @throws ErrorException
      * @throws Exception
-     * @throws NotSupportedException
-     * @throws ServerErrorHttpException
      */
     public function safeUp(): bool
     {
         $installMigration = new SproutBaseSitemapsInstallMigration();
-
-//        if ($this->db->tableExists($oldTable)){
         $installMigration->insertDefaultSettings();
-
-//        }
 
         return true;
     }
